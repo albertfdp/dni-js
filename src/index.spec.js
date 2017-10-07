8b3ed0ad03cbc4089f114d50a531c87698006d8c
Albert Fernández <albert@nandez.cat>
2017-10-07 19:33:38 +0100

    Format tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,18 @@
-const expect = require('unexpected').clone();
+const unexpected = require('unexpected');
 
 const { dni, nie, isNIE, getControlDigit, isValid } = require('.');
 
+const expect = unexpected
+  .clone()
+  .addAssertion(['<any> [not] to be a valid DNI'], (expect, subject) =>
+    expect(isValid(subject), '[not] to be', true)
+  )
+  .addAssertion(
+    ['<any> [not] to have control digit <string>'],
+    (expect, subject, value) => {
+      return expect(getControlDigit(subject), '[not] to equal', value);
+    }
+  );
 
 describe('dni-js', () => {
   describe('dni', () => {
@@ -9,20 +20,20 @@ describe('dni-js', () => {
       expect(dni(12345678), 'to equal', '12345678-Z');
       expect(dni('12345678'), 'to equal', '12345678-Z');
     });
-    
+
     describe('when passing an invalid number', () => {
       it('returns null if DNI contains extra chars', () => {
         expect(dni('12345678FFF'), 'to equal', null);
         expect(dni('X1234567FFF'), 'to equal', null);
-      })
-      
+      });
+
       it('returns null', () => {
         expect(dni(), 'to equal', null);
         expect(dni(2), 'to equal', null);
         expect(dni(''), 'to equal', null);
         expect(dni('1234567'), 'to equal', null);
       });
-    })
+    });
   });
 
   describe('nie', () => {
@@ -34,7 +45,7 @@ describe('dni-js', () => {
   describe('isValid', () => {
     ['12345678-Z', '12345678Z', '12345678 Z', 'X1234567-L'].forEach(number => {
       it(`returns true for valid number ${number}`, () => {
-        expect(isValid(number), 'to be true');
+        expect(number, 'to be a valid DNI');
       });
     });
 
@@ -50,23 +61,23 @@ describe('dni-js', () => {
       12345678
     ].forEach(number => {
       it(`returns false for invalid number ${number}`, () => {
-        expect(isValid(number), 'to be false');
+        expect(number, 'not to be a valid DNI');
       });
     });
   });
 
   describe('getControlDigit', () => {
     it('when passing an integer returns Z', () => {
-      expect(getControlDigit(12345678), 'to equal', 'Z');
+      expect(12345678, 'to have control digit', 'Z');
     });
 
     it('when passing a string returns Z', () => {
-      expect(getControlDigit('12345678'), 'to equal', 'Z');
+      expect('12345678', 'to have control digit', 'Z');
     });
 
     describe('when using a NIE', () => {
       it('when passing an integer returns Z', () => {
-        expect(getControlDigit('X1234567'), 'to equal', 'L');
+        expect('X1234567', 'to have control digit', 'L');
       });
     });
   });
